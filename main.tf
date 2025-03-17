0d51da8044269e92f8677c3acec4894be0ec906f
Vinith Amma <vinithamma007@gmail.com>
2025-03-17 23:52:32 +0530

    tf practice

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_route53_record" "record" {
 }
 
 resource "null_resource" "ansible" {
-  depends_on = [ aws_route53_record.record ]
+  depends_on = [aws_route53_record.record]
   provisioner "local-exec" {
     command = <<EOF
 
