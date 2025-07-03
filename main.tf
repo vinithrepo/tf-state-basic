8a218b6292e81df80e690ea91825377fb9fafa92
Vinith Amma <vinithamma007@gmail.com>
2025-07-03 20:13:54 +0530

    frontend r-ansible

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ resource "aws_route53_record" "record" {
   records    = [aws_instance.instance.private_ip]
 }
 
-resource "null_resource" "ansible" {
-  depends_on = [aws_route53_record.record]
-  provisioner "local-exec" {
-    command = <<EOF
-
-cd /home/centos/r-ansible   #roboshop-ansible
-git pull
-sleep 60
-ansible-playbook -i ${var.name}-dev.vinithaws.online, main.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component=${var.name}
-
-EOF
-  }
-}
+#resource "null_resource" "ansible" {
+#  depends_on = [aws_route53_record.record]
+#  provisioner "local-exec" {
+#    command = <<EOF
+#
+#cd /home/centos/r-ansible   #roboshop-ansible
+#git pull
+#sleep 60
+#ansible-playbook -i ${var.name}-dev.vinithaws.online, main.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component=${var.name}
+#
+#EOF
+#  }
+#}
