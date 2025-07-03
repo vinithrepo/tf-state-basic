41e1a973ce7e9c23b96e76846aa5515f54e7f841
Vinith Amma <vinithamma007@gmail.com>
2025-07-03 21:35:21 +0530

    roboshop-ansible

--- data.tf
+++ data.tf
@@ -1,5 +1,10 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "RHEL-9-DevOps-Practice"
+  name_regex = "Centos-8-DevOps-Practice"
   owners = [ 973714476881 ]
 }
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex = "RHEL-8-DevOps-Practice"
+#  owners = [ 973714476881 ]
+#}
