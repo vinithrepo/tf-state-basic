8be85f0509210055a471db8582e7559ef5bce48e
Vinith Amma <vinithamma007@gmail.com>
2025-07-04 12:57:40 +0530

    roboshop-ansible ami_ id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "instance" {
-  ami                     = data.aws_ami.ami.id
+  ami                     = var.ami_id
   instance_type           = var.instance_type
   vpc_security_group_ids  = var.security_groups
 
