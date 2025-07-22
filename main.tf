db2afd5cc9ffbef455d0e1d6f4e13701441a0333
Vinith Amma <vinithamma007@gmail.com>
2025-07-22 21:42:29 +0530

    app module alb&tg s-42

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
-
 resource "aws_instance" "instance" {
-  ami                     = var.ami_id
-  instance_type           = var.instance_type
-  vpc_security_group_ids  = var.security_groups
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_groups
 
   tags = {
     Name = var.name
   }
 }
 resource "aws_route53_record" "record" {
-  zone_id    = var.zone_id
-  name       = "${var.name}-dev.vinithaws.online"
-  type       = "A"
-  ttl        = 30
-  records    = [aws_instance.instance.private_ip]
+  zone_id = var.zone_id
+  name    = "${var.name}-dev.vinithaws.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
 }
 
 resource "null_resource" "ansible" {
