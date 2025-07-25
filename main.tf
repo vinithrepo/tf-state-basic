ba3fc7d36e8b21f3818379fc3a7ec0f28072a83b
Vinith Amma <vinithamma007@gmail.com>
2025-07-26 00:32:29 +0530

    centos8+tf-ansible

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "null_resource" "ansible" {
 cd /home/centos/r-ansible
 git pull
 sleep 60
-ansible-playbook -i ${var.name}-dev.vinithaws.online, main.yaml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+ansible-playbook -i ${var.name}-dev.vinithaws.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 
 EOF
   }
